14c6f0e82e6453b8b78e765506320426b9839fae
Sangiwe <nokubongasangiwe@gmail.com>
2025-07-03 20:20:31 +0200

    Added Aggregations And Window functions

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,30 @@
+-- COUNT function and GROUP BY clause.
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    COUNT(b.booking_id) AS total_bookings
+FROM 
+    User u
+LEFT JOIN 
+    Booking b ON u.user_id = b.user_id
+GROUP BY 
+    u.user_id, u.first_name, u.last_name
+ORDER BY 
+    total_bookings DESC;
+
+--window function (ROW_NUMBER, RANK)
+SELECT
+    p.property_id,
+    p.name,
+    COUNT(b.booking_id) AS booking_count,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS popularity_rank
+FROM
+    Property p
+LEFT JOIN
+    Booking b ON p.property_id = b.property_id
+GROUP BY
+    p.property_id, p.name
+ORDER BY
+    popularity_rank
+
